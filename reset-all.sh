3dc69f0accc626e30abaf205c1f31e40c7e17d7d
Rodrigo D. Oliveira <rodrigo.oliveira@gethelix.org>
2020-03-30 19:43:55 -0300

    Added mongo collections drop on reset script

--- reset-all.sh
+++ reset-all.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # This script resets everything from Helix.
 clear
+
+mongo -u helix -p H3l1xNG --port 27000 --authenticationDatabase admin main --eval "db.getCollectionNames().forEach(function(n){db[n].drop()});"
+mongo -u helix -p H3l1xNG --port 27000 --authenticationDatabase admin orion --eval "db.getCollectionNames().forEach(function(n){db[n].drop()});"
+mongo -u helix -p H3l1xNG --port 27000 --authenticationDatabase admin orion-helixiot --eval "db.getCollectionNames().forEach(function(n){db[n].drop()});"
+mongo --port 28018 iotagentul --eval "db.getCollectionNames().forEach(function(n){db[n].drop()});"
+
 sudo pkill Helix
 sudo rm *.out
 
