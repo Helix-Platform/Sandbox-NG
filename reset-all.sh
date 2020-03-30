7e643f57c7c1cf6c3be82a0539910e3975b59f2c
Rodrigo D. Oliveira <rodrigo.oliveira@gethelix.org>
2020-03-30 18:31:42 -0300

    Master Reset script

---
+++ reset-all.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# This script resets everything from Helix.
+clear
+sudo pkill Helix
+sudo rm *.out
+
+sudo docker rm -vf $(docker ps -a -q)
+sudo docker rmi -f $(docker images -a -q)
+
+sudo rm -rf /opt/secrets/ssl_crt
+sudo rm -rf /opt/secrets/ssl_key
+
+./install.sh
